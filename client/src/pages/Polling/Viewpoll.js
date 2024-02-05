31ac7f7f9aab730663cbdea359d7a9db7b23b197
Dinesh <dineshkumarpaliwal83@gmail.com>
2024-02-05 18:25:44 +0530

    some pages designed

--- client/src/pages/Polling/Viewpoll.js
+++ client/src/pages/Polling/Viewpoll.js
@@ -78,25 +78,25 @@
 //           >
 //             Options:
 //           </label>
-//           {options.map((option, index) => (
-//             <div key={option.id} style={{ marginBottom: "16px" }}>
-//               <input
-//                 type="radio"
-//                 id={`option-${index}`}
-//                 name="selectedOption"
-//                 value={index}
-//                 checked={selectedOption === index}
-//                 onChange={() => setSelectedOption(index)}
-//                 style={{
-//                   marginRight: "8px",
-//                 }}
-//               />
-//               <label htmlFor={`option-${index}`}>
-//                 {option.options}
-//                 {/* Adjust this line to display the option value */}
-//               </label>
-//             </div>
-//           ))}
+// {options.map((option, index) => (
+//   <div key={option.id} style={{ marginBottom: "16px" }}>
+//     <input
+//       type="radio"
+//       id={`option-${index}`}
+//       name="selectedOption"
+//       value={index}
+//       checked={selectedOption === index}
+//       onChange={() => setSelectedOption(index)}
+//       style={{
+//         marginRight: "8px",
+//       }}
+//     />
+//     <label htmlFor={`option-${index}`}>
+//       {option.options}
+//       {/* Adjust this line to display the option value */}
+//     </label>
+//   </div>
+// ))}
 
 //           <button
 //             type="submit"
@@ -123,14 +123,18 @@
 
 // export default Viewpoll;
 
-// PollComponent.jsx
-
 import React, { useState, useEffect } from "react";
-import "./Viewpoll.css";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import toast from "react-hot-toast";
 import { useAuth } from "../../context/auth";
+import { Radio } from "@mui/material";
+import "./Viewpoll.css";
+import Button from "@mui/material/Button";
+
+import PollIcon from "@mui/icons-material/Poll";
+
+import { RadioGroup, FormControlLabel } from "@mui/material";
 
 const Viewpoll = () => {
   const navigate = useNavigate();
@@ -141,10 +145,6 @@ const Viewpoll = () => {
   const [pollId, setPollId] = useState("");
   const [validuser, setValidUser] = useState(0);
   const username = auth?.user?.name;
-
-  const handleOptionClick = (index) => {
-    setSelectedOption(index === selectedOption ? null : index);
-  };
 
   useEffect(() => {
     // Fetch existing poll data if pollId is provided
@@ -177,67 +177,82 @@ const Viewpoll = () => {
         selectedOption: options[selectedOption],
         username,
       });
+      toast.success("Poll submitted successfully");
       console.log("Poll updated successfully:", response.data);
+
       navigate("/dashboard/student");
     } catch (error) {
+      toast.error("Error submitting poll");
       console.error("Error submitting poll:", error);
     }
   };
   console.log(`your id is${validuser}`);
-
   return (
-    <div>
-      {validuser === 0 && pollId ? (
-        <>
-          <div className="wrapper">
-            <header>Poll UI Design</header>
-            <form onSubmit={handleSubmit}>
-              <div className="poll-area">
-                {options.map((option, index) => (
-                  <label
-                    key={option.id}
-                    className={`opt-${index} ${
-                      selectedOption === index - 1 ? "selected" : ""
-                    }`}
-                    onClick={() => handleOptionClick(index - 1)}
-                  >
-                    <div className="row">
-                      <div className="column">
-                        <span className="circle"></span>
-                        <span className="text">
-                          {index}.{option.options}
-                        </span>
-                      </div>
-                      <span className="percent">{option.count}</span>
-                    </div>
-                    <div
-                      className="progress"
-                      style={{ "--w": 30 * index }}
-                    ></div>
-                  </label>
-                ))}
-              </div>
-              <button
-                type="submit"
-                style={{
-                  backgroundColor: "#4caf50",
-                  color: "#fff",
-                  padding: "10px",
-                  border: "none",
-                  borderRadius: "4px",
-                  cursor: "pointer",
-                }}
-              >
-                Submit
-              </button>
-            </form>
-          </div>
-        </>
-      ) : validuser === 1 ? (
-        <p>You have done polling</p>
-      ) : (
-        <p>No poll available</p>
-      )}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+        marginTop: "50px",
+      }}
+    >
+      <div className="wrappers">
+        {validuser === 0 && pollId ? (
+          <form onSubmit={handleSubmit} className="poll-area">
+            <p style={{ fontWeight: "bold", marginTop: 100, fontSize: 30 }}>
+              {question} ?{" "}
+            </p>
+            <hr />
+
+            <RadioGroup
+              aria-labelledby="demo-controlled-radio-buttons-group"
+              name="controlled-radio-buttons-group"
+              value={selectedOption}
+              onChange={(event) => setSelectedOption(event.target.value)}
+            >
+              {options.map((option, index) => (
+                <FormControlLabel
+                  key={index}
+                  value={index}
+                  control={<Radio />}
+                  label={
+                    <>
+                      {`${option.options} `}
+                      <PollIcon style={{ marginLeft: "160px" }} />
+                      {` (${option.count})`}
+                    </>
+                  }
+                />
+              ))}
+            </RadioGroup>
+
+            <hr />
+            {/* <button
+             
+              style={{
+                backgroundColor: "blue",
+                color: "white",
+                padding: "10px",
+                border: "none",
+                borderRadius: "4px",
+                cursor: "pointer",
+                width: "200px",
+                margin: "0 auto",
+              }}
+            >
+              Vote
+            </button> */}
+
+            <Button type="submit" variant="contained">
+              Vote
+            </Button>
+          </form>
+        ) : validuser === 1 ? (
+          <p>You have done polling</p>
+        ) : (
+          <p>No poll available</p>
+        )}
+      </div>
     </div>
   );
 };
--- client/src/pages/user/profile.js
+++ client/src/pages/user/profile.js
@@ -1,7 +1,270 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { useState } from "react";
+// import { useAuth } from "../../context/auth";
+// import axios from "axios";
+// import { NavLink } from "react-router-dom";
+
+// const Profile = () => {
+// const [avatar, setAvatar] = useState(null);
+// const [auth, setAuth] = useAuth();
+// const [singleUser, setSingleUser] = useState(null);
+// const [phone, setPhone] = useState("");
+// const [year, setYear] = useState("");
+// const [loading, setLoading] = useState(true);
+
+// const currUser = auth.user;
+
+// useEffect(() => {
+//   const fetchData = async () => {
+//     if (auth.user && auth.user._id) {
+//       try {
+//         const response = await axios.get(
+//           `/api/v1/general/myprofile?userId=${auth.user._id}`
+//         );
+//         setSingleUser(response.data);
+//         setPhone(response.data.phone);
+//         setYear(response.data.year);
+//         setLoading(false);
+//       } catch (error) {
+//         console.error("Error fetching user data:", error);
+//         setLoading(false);
+//       }
+//     }
+//   };
+
+//   fetchData();
+// }, [auth.user]);
+
+// const handleFileChange = (e) => {
+//   const file = e.target.files[0];
+//   setAvatar(file);
+// };
+
+// const handleSave = async () => {
+//   try {
+//     const id = singleUser._id;
+//     const response = await axios.put(
+//       `/api/v1/general/updateProfile/${id}`,
+//       { phone, year },
+//       {
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
+//     console.log("Profile data saved:", response.data);
+//     window.alert("profile updated successfully");
+//   } catch (error) {
+//     window.alert("Profile is not updated");
+//     console.error("Error saving profile data:", error);
+//   }
+// };
+
+// const handleYearChange = (e) => {
+//   setYear(e.target.value);
+// };
+
+// const handlePhoneChange = (e) => {
+//   setPhone(e.target.value);
+// };
+
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center",
+//         height: "100vh",
+//       }}
+//     >
+//       {loading ? (
+//         <p>Loading...</p>
+//       ) : (
+//         <div
+//           className="container bootstrap snippets bootdey"
+//           style={{ maxWidth: "800px", margin: "50px auto" }}
+//         >
+//           <h1 className="text-primary text-center mb-4">Edit Profile</h1>
+//           <div className="row">
+//             <div className="col-md-3">
+//               <div className="text-center">
+//                 <img
+//                   src="https://bootdey.com/img/Content/avatar/avatar7.png"
+//                   className="avatar img-circle img-thumbnail"
+//                   alt="avatar"
+//                   style={{ maxWidth: "100%", height: "auto" }}
+//                 />
+//                 <h6 className="mt-3">Upload a different photo...</h6>
+//                 <input
+//                   type="file"
+//                   className="form-control"
+//                   onChange={handleFileChange}
+//                 />
+//               </div>
+//             </div>
+
+//             <div className="col-md-9 personal-info">
+//               <h2>Personal info</h2>
+//               <form className="form-horizontal" role="form">
+//                 <div className="form-group mb-3">
+//                   <label
+//                     className="col-lg-3 control-label"
+//                     style={{ fontWeight: "bold" }}
+//                   >
+//                     Name
+//                   </label>
+//                   <div className="col-lg-8">
+//                     <input
+//                       className="form-control form-control-lg"
+//                       type="text"
+//                       value={currUser.name}
+//                       readOnly
+//                     />
+//                   </div>
+//                 </div>
+//                 <div className="form-group mb-3">
+//                   <label
+//                     className="col-lg-3 control-label"
+//                     style={{ fontWeight: "bold" }}
+//                   >
+//                     Reg No.
+//                   </label>
+//                   <div className="col-lg-8">
+//                     <input
+//                       className="form-control form-control-lg"
+//                       type="text"
+//                       value={singleUser ? singleUser.reg : ""}
+//                       readOnly
+//                     />
+//                   </div>
+//                 </div>
+//                 <div className="form-group mb-3">
+//                   <label
+//                     className="col-lg-3 control-label"
+//                     style={{ fontWeight: "bold" }}
+//                   >
+//                     Email
+//                   </label>
+//                   <div className="col-lg-8">
+//                     <input
+//                       className="form-control form-control-lg"
+//                       type="text"
+//                       value={currUser.email}
+//                       readOnly
+//                     />
+//                   </div>
+//                 </div>
+//                 <div className="form-group mb-3">
+//                   <label
+//                     className="col-lg-3 control-label"
+//                     style={{ fontWeight: "bold" }}
+//                   >
+//                     Phone No.:
+//                   </label>
+//                   <div className="col-lg-8">
+//                     <input
+//                       className="form-control form-control-lg"
+//                       type="text"
+//                       value={phone}
+//                       onChange={handlePhoneChange}
+//                     />
+//                   </div>
+//                 </div>
+//                 <div className="form-group mb-3">
+//                   <label
+//                     className="col-lg-3 control-label"
+//                     style={{ fontWeight: "bold" }}
+//                   >
+//                     Year
+//                   </label>
+//                   <div className="col-lg-8">
+//                     <select
+//                       className="form-control form-control-lg"
+//                       value={year}
+//                       onChange={handleYearChange}
+//                     >
+//                       <option value="1">1st Year</option>
+//                       <option value="2">2nd Year</option>
+//                       <option value="3">3rd Year</option>
+//                       <option value="4">4th Year</option>
+//                     </select>
+//                   </div>
+//                 </div>
+//                 <div className="form-group mb-3">
+//                   <label
+//                     className="col-lg-3 control-label"
+//                     style={{ fontWeight: "bold" }}
+//                   >
+//                     Hostel
+//                   </label>
+//                   <div className="col-lg-8">
+//                     <input
+//                       className="form-control form-control-lg"
+//                       type="text"
+//                       value={currUser.hostel}
+//                       readOnly
+//                     />
+//                   </div>
+//                 </div>
+
+//                 <div className="form-group mb-3">
+//                   <div className="col-lg-offset-3 col-lg-8">
+//                     <button
+//                       type="button"
+//                       className="btn btn-primary btn-lg"
+//                       onClick={handleSave}
+//                     >
+//                       Save
+//                     </button>
+//                   </div>
+//                 </div>
+
+//                 <NavLink to="/dashboard/student/profile/payment">
+//                   <button type="button" className="btn btn-danger btn-lg">
+//                     Payment
+//                   </button>
+//                 </NavLink>
+//                 {singleUser && singleUser.paid === "1" ? (
+//                   <span
+//                     style={{
+//                       margin: "0 10px",
+//                       fontWeight: "bold",
+//                       fontSize: "1.2rem",
+//                     }}
+//                   >
+//                     Status: Paid{singleUser.verified ? " and Verified" : ""}
+//                   </span>
+//                 ) : (
+//                   <span
+//                     style={{
+//                       margin: "0 10px",
+//                       fontWeight: "bold",
+//                       fontSize: "1.2rem",
+//                     }}
+//                   >
+//                     Status: Unpaid
+//                   </span>
+//                 )}
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Profile;
+
+// RegistrationForm.js
+
+import React from "react";
+import "./../../styles/profile.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import { useAuth } from "../../context/auth";
+import { useEffect } from "react";
 import axios from "axios";
 import { NavLink } from "react-router-dom";
 
@@ -68,190 +331,110 @@ const Profile = () => {
   const handlePhoneChange = (e) => {
     setPhone(e.target.value);
   };
-
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
+    <>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <div
-          className="container bootstrap snippets bootdey"
-          style={{ maxWidth: "800px", margin: "50px auto" }}
-        >
-          <h1 className="text-primary text-center mb-4">Edit Profile</h1>
-          <div className="row">
-            <div className="col-md-3">
-              <div className="text-center">
-                <img
-                  src="https://bootdey.com/img/Content/avatar/avatar7.png"
-                  className="avatar img-circle img-thumbnail"
-                  alt="avatar"
-                  style={{ maxWidth: "100%", height: "auto" }}
-                />
-                <h6 className="mt-3">Upload a different photo...</h6>
-                <input
-                  type="file"
-                  className="form-control"
-                  onChange={handleFileChange}
-                />
+        <div className="container">
+          <div className="title">Update Profile</div>
+          <div className="content">
+            <form action="#" onSubmit={handleSave}>
+              <div className="user-details">
+                <div className="input-box">
+                  <span className="details">Name</span>
+                  <input
+                    className="form-control form-control-lg"
+                    type="text"
+                    value={currUser.name}
+                    readOnly
+                  />
+                </div>
+                <div className="input-box">
+                  <span className="details">Registration Number</span>
+                  <input
+                    className="form-control form-control-lg"
+                    type="text"
+                    value={singleUser ? singleUser.reg : ""}
+                    readOnly
+                  />
+                </div>
+                <div className="input-box">
+                  <span className="details">Email</span>
+                  <input
+                    className="form-control form-control-lg"
+                    type="text"
+                    value={currUser.email}
+                    readOnly
+                  />
+                </div>
+                <div className="input-box">
+                  <span className="details">Phone Number</span>
+                  <input
+                    className="form-control form-control-lg"
+                    type="text"
+                    value={phone}
+                    onChange={handlePhoneChange}
+                  />
+                </div>
+                <div className="input-box">
+                  <span className="details">Year</span>
+                  <select
+                    className="form-control form-control-lg"
+                    value={year}
+                    onChange={handleYearChange}
+                  >
+                    <option value="1">1st Year</option>
+                    <option value="2">2nd Year</option>
+                    <option value="3">3rd Year</option>
+                    <option value="4">4th Year</option>
+                  </select>
+                </div>
+                <div className="input-box">
+                  <span className="details">Hostel</span>
+                  <input
+                    className="form-control form-control-lg"
+                    type="text"
+                    value={currUser.hostel}
+                    readOnly
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="col-md-9 personal-info">
-              <h2>Personal info</h2>
-              <form className="form-horizontal" role="form">
-                <div className="form-group mb-3">
-                  <label
-                    className="col-lg-3 control-label"
-                    style={{ fontWeight: "bold" }}
-                  >
-                    Name
-                  </label>
-                  <div className="col-lg-8">
-                    <input
-                      className="form-control form-control-lg"
-                      type="text"
-                      value={currUser.name}
-                      readOnly
-                    />
-                  </div>
-                </div>
-                <div className="form-group mb-3">
-                  <label
-                    className="col-lg-3 control-label"
-                    style={{ fontWeight: "bold" }}
-                  >
-                    Reg No.
-                  </label>
-                  <div className="col-lg-8">
-                    <input
-                      className="form-control form-control-lg"
-                      type="text"
-                      value={singleUser ? singleUser.reg : ""}
-                      readOnly
-                    />
-                  </div>
-                </div>
-                <div className="form-group mb-3">
-                  <label
-                    className="col-lg-3 control-label"
-                    style={{ fontWeight: "bold" }}
-                  >
-                    Email
-                  </label>
-                  <div className="col-lg-8">
-                    <input
-                      className="form-control form-control-lg"
-                      type="text"
-                      value={currUser.email}
-                      readOnly
-                    />
-                  </div>
-                </div>
-                <div className="form-group mb-3">
-                  <label
-                    className="col-lg-3 control-label"
-                    style={{ fontWeight: "bold" }}
-                  >
-                    Phone No.:
-                  </label>
-                  <div className="col-lg-8">
-                    <input
-                      className="form-control form-control-lg"
-                      type="text"
-                      value={phone}
-                      onChange={handlePhoneChange}
-                    />
-                  </div>
-                </div>
-                <div className="form-group mb-3">
-                  <label
-                    className="col-lg-3 control-label"
-                    style={{ fontWeight: "bold" }}
-                  >
-                    Year
-                  </label>
-                  <div className="col-lg-8">
-                    <select
-                      className="form-control form-control-lg"
-                      value={year}
-                      onChange={handleYearChange}
-                    >
-                      <option value="1">1st Year</option>
-                      <option value="2">2nd Year</option>
-                      <option value="3">3rd Year</option>
-                      <option value="4">4th Year</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="form-group mb-3">
-                  <label
-                    className="col-lg-3 control-label"
-                    style={{ fontWeight: "bold" }}
-                  >
-                    Hostel
-                  </label>
-                  <div className="col-lg-8">
-                    <input
-                      className="form-control form-control-lg"
-                      type="text"
-                      value={currUser.hostel}
-                      readOnly
-                    />
-                  </div>
-                </div>
-
-                <div className="form-group mb-3">
-                  <div className="col-lg-offset-3 col-lg-8">
-                    <button
-                      type="button"
-                      className="btn btn-primary btn-lg"
-                      onClick={handleSave}
-                    >
-                      Save
-                    </button>
-                  </div>
-                </div>
-
-                <NavLink to="/dashboard/student/profile/payment">
-                  <button type="button" className="btn btn-danger btn-lg">
-                    Payment
-                  </button>
-                </NavLink>
-                {singleUser && singleUser.paid === "1" ? (
-                  <span
-                    style={{
-                      margin: "0 10px",
-                      fontWeight: "bold",
-                      fontSize: "1.2rem",
-                    }}
-                  >
-                    Status: Paid{singleUser.verified ? " and Verified" : ""}
-                  </span>
-                ) : (
-                  <span
-                    style={{
-                      margin: "0 10px",
-                      fontWeight: "bold",
-                      fontSize: "1.2rem",
-                    }}
-                  >
-                    Status: Unpaid
-                  </span>
-                )}
-              </form>
-            </div>
+              <div className="button">
+                <input type="submit" value="Save" />
+              </div>
+              <NavLink to="/dashboard/student/profile/payment">
+                <button type="button" className="btn btn-danger btn-lg">
+                  Payment
+                </button>
+              </NavLink>
+              {singleUser && singleUser.paid === "1" ? (
+                <span
+                  style={{
+                    margin: "0 10px",
+                    fontWeight: "bold",
+                    fontSize: "1.2rem",
+                  }}
+                >
+                  Status: Paid{singleUser.verified ? " and Verified" : ""}
+                </span>
+              ) : (
+                <span
+                  style={{
+                    margin: "0 10px",
+                    fontWeight: "bold",
+                    fontSize: "1.2rem",
+                  }}
+                >
+                  Status: Unpaid
+                </span>
+              )}
+            </form>
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
